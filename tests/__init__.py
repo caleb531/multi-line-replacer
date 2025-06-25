cf02054cd24f5e6d54790725685c36a8b556b644
Caleb Evans <caleb@calebevans.me>
2025-06-24 18:04:36 -0700

    Use Path objects in custom MLRTestCase
    Also refactor assert_file_replace() to be more in line with the CLI
    argument syntax.

--- tests/__init__.py
+++ tests/__init__.py
@@ -16,7 +16,7 @@ from mlr.__main__ import main
 
 # The temporary working directory in which the tests can safely perform file
 # modifications
-temp_dir_path = os.path.join(tempfile.gettempdir(), "mlr-test-data")
+temp_dir_path = Path(tempfile.gettempdir()) / "mlr-test-data"
 # Subdirectories from anywhere in the project to copy to the temporary working
 # directory
 subdirs_to_copy = ("tests/input", "tests/output", "tests/rules")
@@ -31,7 +31,7 @@ class MLRTestCase(unittest.TestCase):
     def setUp(self):
         for subdir_path in subdirs_to_copy:
             subdir_name = os.path.basename(subdir_path)
-            temp_subdir_path = os.path.join(temp_dir_path, subdir_name)
+            temp_subdir_path = temp_dir_path / subdir_name
             with contextlib.suppress(OSError):
                 os.makedirs(temp_subdir_path)
                 shutil.copytree(subdir_path, temp_subdir_path, dirs_exist_ok=True)
@@ -42,35 +42,27 @@ class MLRTestCase(unittest.TestCase):
             shutil.rmtree(temp_dir_path)
 
     def get_input_path(self, input_filename):
-        return os.path.join(temp_dir_path, "input", input_filename)
+        return temp_dir_path / "input" / input_filename
 
     def get_rule_path(self, input_filename):
-        return os.path.join(temp_dir_path, "rules", input_filename)
+        return temp_dir_path / "rules" / input_filename
 
     def get_output_path(self, input_filename):
-        return os.path.join(temp_dir_path, "output", input_filename)
+        return temp_dir_path / "output" / input_filename
 
-    def assert_file_replace(self, *operations):
+    def assert_file_replace(self, input_filenames, rule_filenames, output_filenames):
         with patch(
             "sys.argv",
             [
                 __file__,
-                *(
-                    self.get_input_path(operation["input_filename"])
-                    for operation in operations
-                ),
+                *(str(self.get_input_path(f)) for f in input_filenames),
                 "-r",
-                *(
-                    self.get_rule_path(operation["rule_filename"])
-                    for operation in operations
-                ),
+                *(str(self.get_rule_path(f)) for f in rule_filenames),
             ],
         ):
             main()
-            for operation in operations:
+            for input_file, output_file in zip(input_filenames, output_filenames):
                 self.assertEqual(
-                    Path(
-                        self.get_output_path(operation["output_filename"])
-                    ).read_text(),
-                    Path(self.get_input_path(operation["input_filename"])).read_text(),
+                    self.get_output_path(output_file).read_text(),
+                    self.get_input_path(input_file).read_text(),
                 )
--- tests/test_replacer.py
+++ tests/test_replacer.py
@@ -7,29 +7,23 @@ class TestMLR(MLRTestCase):
     def test_literal_replacement(self):
         """Should perform a literal textual replacement"""
         self.assert_file_replace(
-            {
-                "input_filename": "test.editorconfig",
-                "rule_filename": "editorconfig.md",
-                "output_filename": "test.editorconfig",
-            }
+            input_filenames=["test.editorconfig"],
+            rule_filenames=["editorconfig.md"],
+            output_filenames=["test.editorconfig"],
         )
 
     def test_match_all_but_newline(self):
         """should perform a replacement with MATCH_ALL_BUT_NEWLINE"""
         self.assert_file_replace(
-            {
-                "input_filename": "lint.yml",
-                "rule_filename": "gha/ruff.md",
-                "output_filename": "lint-ruff.yml",
-            }
+            input_filenames=["lint.yml"],
+            rule_filenames=["gha/ruff.md"],
+            output_filenames=["lint-ruff.yml"],
         )
 
     def test_match_all_but_newline_lazy(self):
         """should perform a replacement with MATCH_ALL_BUT_NEWLINE_LAZY"""
         self.assert_file_replace(
-            {
-                "input_filename": "lint.yml",
-                "rule_filename": "gha/python-version.md",
-                "output_filename": "lint-python-version.yml",
-            }
+            input_filenames=["lint.yml"],
+            rule_filenames=["gha/python-version.md"],
+            output_filenames=["lint-python-version.yml"],
         )
