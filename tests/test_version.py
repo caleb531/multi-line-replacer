a3a5441dd8cfc7f3bdc4cb870cffe489eabb7f05
Caleb Evans <caleb@calebevans.me>
2026-02-06 13:07:55 -0800

    Clean up version tests

--- tests/test_version.py
+++ tests/test_version.py
@@ -3,8 +3,7 @@
 import contextlib
 import importlib.metadata
 import pathlib
-import sys
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 
 import pytest
 import tomli
@@ -12,6 +11,7 @@ import tomli
 from mlr.__main__ import main
 
 
+@patch("sys.argv", ["mlr", "--version"])
 def test_version_matches_pyproject(capsys: pytest.CaptureFixture) -> None:
     """
     The printed version should match the version in pyproject.toml
@@ -19,25 +19,24 @@ def test_version_matches_pyproject(capsys: pytest.CaptureFixture) -> None:
     pyproject_data = tomli.loads(pathlib.Path("pyproject.toml").read_text())
     expected_version = pyproject_data["project"]["version"]
 
-    with patch.object(sys, "argv", ["mlr", "--version"]):
-        with contextlib.suppress(SystemExit):
-            main()
+    with contextlib.suppress(SystemExit):
+        main()
 
     captured = capsys.readouterr()
     assert expected_version in captured.out
 
 
-def test_version_fallback(capsys: pytest.CaptureFixture) -> None:
+@patch(
+    "importlib.metadata.version",
+    side_effect=importlib.metadata.PackageNotFoundError,
+)
+@patch("sys.argv", ["mlr", "--version"])
+def test_version_fallback(version: MagicMock, capsys: pytest.CaptureFixture) -> None:
     """
     The version should default to 0.0.0 if the package metadata cannot be found
     """
-    with patch(
-        "importlib.metadata.version",
-        side_effect=importlib.metadata.PackageNotFoundError,
-    ):
-        with patch.object(sys, "argv", ["mlr", "--version"]):
-            with contextlib.suppress(SystemExit):
-                main()
+    with contextlib.suppress(SystemExit):
+        main()
 
     captured = capsys.readouterr()
     assert "0.0.0" in captured.out
